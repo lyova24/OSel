00c7d3f0ac929b8737d670c489e2bb15bc99c6c5
lyova24 <lengthylyova@gmail.com>
2025-05-30 06:40:24 +0700

    core

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 #![no_std]
 #![no_main]
 
+static GREETING: &[u8] = b"WELCOME TO MY pidorustOS!";
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in GREETING.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        };
+    }
     loop {}
 }
 
